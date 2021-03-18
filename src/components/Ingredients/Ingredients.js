f1daa98415a7e250ea195d105cb761beb68f86a2
Marina294 <69162947+Marina294@users.noreply.github.com>
2021-03-18 02:39:32 -0700

    2nd

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,18 +6,61 @@ import Search from './Search';
 
 
 function Ingredients() {
-
   const [userIngredients, setUserIngredients] = useState([])
+
+  // useEffect(() => {}, [])
+  useEffect(() => {
+    fetch('https://react-hooks-update-cea5e-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData){
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+        })
+        }
+        setUserIngredients(loadedIngredients)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log('Rendering Ingredients...', userIngredients)
+    // return () => {
+    //   console.log('This component will be unmounted....')
+    // }
+  }, [userIngredients])
 
   const addIngredientHandler = (ingredients) => {
     // NO
     // setUserIngredients(Ingredients)
     // setUserIngredients(userIngredients.push(Ingredients))
-    setUserIngredients((prevState) => [
+    // Ok
+    // setUserIngredients((prevState) => [
+    //   ...prevState,
+    //   { id: Math.random().toString(), ...ingredients }
+    // ])
+    fetch('https://react-hooks-update-cea5e-default-rtdb.firebaseio.com/ingredients.json',{
+      method: 'POST',
+      body: JSON.stringify(ingredients),
+      headers: {'Content-Type': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(responseData => {
+      // console.log(responseData)
+      setUserIngredients((prevState) => [
       ...prevState,
-      { id: Math.random().toString(), ...ingredients }
-    ])
+      { id:responseData.name , ...ingredients }
+      ])
+    })
   }
+
+  // const removeIngredientHandler = ingredientId => {
+  //   setUserIngredients(prevState => {
+  //     prevState.filter(ingredient => ingredient.id !== ingredientId)
+  //   })
+  // }
 
 
   return (
@@ -28,6 +71,7 @@ function Ingredients() {
         <Search />
         {/* Need to add list here! */}
         <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
+        {/* <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} /> */}
       </section>
     </div>
   );
